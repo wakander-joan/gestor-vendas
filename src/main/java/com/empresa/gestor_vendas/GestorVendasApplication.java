cd5927b810d2ae213b233b6c471fa2aff48cda96
Joan Reis <joansantosreis@gmail.com>
2024-12-21 15:34:18 -0300

    primeiro commit

---
+++ src/main/java/com/empresa/gestor_vendas/GestorVendasApplication.java
@@ -0,0 +1,13 @@
+package com.empresa.gestor_vendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorVendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestorVendasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/empresa/gestor_vendas/GestorVendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.empresa.gestor_vendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorVendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
