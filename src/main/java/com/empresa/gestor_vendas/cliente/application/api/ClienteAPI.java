358c0a1d32aea2009137768133879b8ce21cb27f
Joan Reis <joansantosreis@gmail.com>
2024-12-21 19:22:35 -0300

    Implementada camada API - busca-Todos-Clientes

--- src/main/java/com/empresa/gestor_vendas/cliente/application/api/ClienteAPI.java
+++ src/main/java/com/empresa/gestor_vendas/cliente/application/api/ClienteAPI.java
@@ -1,12 +1,14 @@
 package com.empresa.gestor_vendas.cliente.application.api;
 
 import com.empresa.gestor_vendas.cliente.application.api.dto.ClienteDetalhadoResponse;
+import com.empresa.gestor_vendas.cliente.application.api.dto.ClienteListResponse;
 import com.empresa.gestor_vendas.cliente.application.api.dto.ClienteRequest;
 import com.empresa.gestor_vendas.cliente.application.api.dto.ClienteResponse;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -21,4 +23,7 @@ public interface ClienteAPI {
     @ResponseStatus(code = HttpStatus.OK)
     ClienteDetalhadoResponse buscaCliente (@PathVariable UUID idCliente);
 
+    @GetMapping("/buscaTodosClientes")
+    @ResponseStatus(code = HttpStatus.OK)
+    List<ClienteListResponse> buscaTodosClientes();
 }
--- src/main/java/com/empresa/gestor_vendas/cliente/application/api/ClienteController.java
+++ src/main/java/com/empresa/gestor_vendas/cliente/application/api/ClienteController.java
@@ -1,6 +1,7 @@
 package com.empresa.gestor_vendas.cliente.application.api;
 
 import com.empresa.gestor_vendas.cliente.application.api.dto.ClienteDetalhadoResponse;
+import com.empresa.gestor_vendas.cliente.application.api.dto.ClienteListResponse;
 import com.empresa.gestor_vendas.cliente.application.api.dto.ClienteRequest;
 import com.empresa.gestor_vendas.cliente.application.api.dto.ClienteResponse;
 import com.empresa.gestor_vendas.cliente.application.service.ClienteService;
@@ -8,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -30,5 +32,12 @@ public class ClienteController implements ClienteAPI {
         ClienteDetalhadoResponse cliente =clienteService.buscaCliente(idCliente);
         log.info("[finish] ClienteController - buscaCliente");
         return cliente;
+    }
+
+    @Override
+    public List<ClienteListResponse> buscaTodosClientes() {
+        log.info("[start] ClienteController - buscaTodosClientes");
+        log.info("[finish] ClienteController - buscaTodosClientes");
+        return List.of();
     }
 }
