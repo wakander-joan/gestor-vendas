c5dbb9775b9577e64f60a9e10f90b542b76b2847
Joan Reis <joansantosreis@gmail.com>
2024-12-21 18:01:46 -0300

    implementa camada API - busca-cliente

--- src/main/java/com/empresa/gestor_vendas/cliente/application/api/ClienteAPI.java
+++ src/main/java/com/empresa/gestor_vendas/cliente/application/api/ClienteAPI.java
@@ -1,10 +1,13 @@
 package com.empresa.gestor_vendas.cliente.application.api;
 
+import com.empresa.gestor_vendas.cliente.application.api.dto.ClienteDetalhadoResponse;
 import com.empresa.gestor_vendas.cliente.application.api.dto.ClienteRequest;
 import com.empresa.gestor_vendas.cliente.application.api.dto.ClienteResponse;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping ("/cliente")
@@ -13,5 +16,9 @@ public interface ClienteAPI {
     @PostMapping("/cadastraCliente")
     @ResponseStatus(code = HttpStatus.CREATED)
     ClienteResponse cadastraCliente (@Valid @RequestBody ClienteRequest clienteRequest);
+
+    @GetMapping("/buscaCliente/{idCliente}")
+    @ResponseStatus(code = HttpStatus.OK)
+    ClienteDetalhadoResponse getCliente (@PathVariable UUID idCliente);
 
 }
--- src/main/java/com/empresa/gestor_vendas/cliente/application/api/ClienteController.java
+++ src/main/java/com/empresa/gestor_vendas/cliente/application/api/ClienteController.java
@@ -1,11 +1,14 @@
 package com.empresa.gestor_vendas.cliente.application.api;
 
+import com.empresa.gestor_vendas.cliente.application.api.dto.ClienteDetalhadoResponse;
 import com.empresa.gestor_vendas.cliente.application.api.dto.ClienteRequest;
 import com.empresa.gestor_vendas.cliente.application.api.dto.ClienteResponse;
 import com.empresa.gestor_vendas.cliente.application.service.ClienteService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
 
 @RestController
 @Log4j2
@@ -19,5 +22,12 @@ public class ClienteController implements ClienteAPI {
         ClienteResponse cliente = clienteService.cadastraCliente(clienteRequest);
         log.info("[finish] ClienteController - cadastraCliente");
         return cliente;
+    }
+
+    @Override
+    public ClienteDetalhadoResponse getCliente(UUID idCliente) {
+        log.info("[start] ClienteController - getCliente");
+        log.info("[finish] ClienteController - getCliente");
+        return null;
     }
 }
